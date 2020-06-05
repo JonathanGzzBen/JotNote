55a1d6c90bb3d877f3a80e30f50d117c5ede63a1
Jonathan Gzz <jonathangzzben@gmail.com>
2020-06-05 17:00:03 -0500

    Add sqlite interaction

--- jotnote/app.py
+++ jotnote/app.py
@@ -2,21 +2,85 @@
 
 import argparse
 import click
+import sqlite3
+import os
 from jotnote.note import Note
+
+database_filename = "jotnote.db"
+
+def create_database():
+    try:
+        sqliteConnection = sqlite3.connect(database_filename)
+        sqlite_create_table_query = """
+            CREATE TABLE IF NOT EXISTS Note (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                content TEXT NOT NULL
+            );
+        """
+        cursor = sqliteConnection.cursor()
+        cursor.execute(sqlite_create_table_query)
+        sqliteConnection.commit()
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Error while creating a sqlite table", error)
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
+    
+
+def save_note(title, content):
+    try:
+        if not os.path.exists(database_filename):
+            create_database()
+        sqliteConnection = sqlite3.connect(database_filename)
+        cursor = sqliteConnection.cursor()
+
+        sqlite_insert_query = f"""
+            INSERT INTO Note
+            (title, content)
+            VALUES
+            ('{title}', '{content}')
+        """
+        cursor.execute(sqlite_insert_query)
+        sqliteConnection.commit()
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Failed to insert data into sqlite table", error)
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
+
+def print_notes():
+    try:
+        sqliteConnection = sqlite3.connect(database_filename)
+        cursor = sqliteConnection.cursor()
+
+        sqlite_select_query = """ SELECT * FROM Note """
+        cursor.execute(sqlite_select_query)
+        records = cursor.fetchall()
+        for row in records:
+            print(f"Title: {row[1]}")
+            print(f"Content: {row[2]}")
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Failes to read data from sqlite table", error)
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx=None):
     if ctx.invoked_subcommand is None:
-        click.echo("List notes")
+        print_notes()
 
-def addWithEditor():
+def add_with_editor():
     content = click.edit()
     content = "".join(content)
     title = content.split(".")[0]
     content = content.split(".")[1]
-    click.echo("Title: " + title)
-    click.echo("Content: " + content)
+    save_note(title, content)
 
 @cli.command()
 @click.argument('content', nargs=-1)
@@ -25,10 +89,9 @@ def add(content):
         content = " ".join(content)
         title = content.split(".")[0]
         content = content.split(".")[1]
-        click.echo("Title: " + title)
-        click.echo("Content: " + content)
+        save_note(title, content)
     else:
-        addWithEditor()
+        add_with_editor()
 
 if __name__ == "__main__":
     cli()
